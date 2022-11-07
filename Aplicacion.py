0e11de6a230f8bccff62106c9d6597c327377202
marcossaira <marcos.saira.95@gmail.com>
2022-11-07 10:25:09 -0500

    Enviado por Saira

--- Aplicacion.py
+++ Aplicacion.py
@@ -1,4 +1,24 @@
+import sqlite3
+from pathlib import Path
+def crear():
+    conexion=sqlite3.connect("SAIRA_almacen.db")
+
+    #En una cadena vamos a guardar la creacion del script  de la tabla.
+    tabla_producto="""CREATE TABLE producto(
+                    idproducto INTEGER PRIMARY KEY AUTOINCREMENT,
+                    codigo TEXT,
+                    nombre TEXT,
+                    precio FLOAT)
+                    """
+    #PARA LAS TABLAS
+    cursor=conexion.cursor()
+    cursor.execute(tabla_producto)
+    conexion.close()
+
+
 def menu():
+    if Path("SAIRA_almacen.db").exists()==False:
+        crear()  
     print("Menu opciones")
     print("1.Registrar")
     print("2.Eliminar")
@@ -6,5 +26,4 @@ def menu():
     print("4.Listar")
     print("5.Salir")
     
-
 menu()
